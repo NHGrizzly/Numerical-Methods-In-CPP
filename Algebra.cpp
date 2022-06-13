53ce9b5b443cda6854d8d490ee7bae02de0bdedb
NHGrizzly <60353404+NHGrizzly@users.noreply.github.com>
2022-06-12 20:17:28 -0400

    Expr origin; Polynomial origin; Monomial tests
    - Monomial tested successfully
    - Polynomial created
    - superclass Expr created
    - Monomial addition ready for test
    - Polynomial addition ready for test

--- Algebra.cpp
+++ Algebra.cpp
@@ -1,40 +1,49 @@
 #include <cmath>
 #include <map>
+#include <iostream>
 
 namespace Algebra {
 
 // base namespace, change later
 
-    class Monomial {
+    class Expr {
 
-    // store and handle evaluation of simple functions
+    };
+
+    class Monomial : public Expr {
+
+    // base unit to store and handle evaluation of simple functions
         
     private:
-        double coeff;
+        double _coeff;
         double exponent;
 
     public:
         Monomial(double coeff, double exponent) {
-            this->coeff = coeff;
+            this->_coeff = coeff;
             this-> exponent = exponent;
         }
 
         double evaluateAt(double x) {
-            return coeff * (pow(x, exponent));
+            return _coeff * (pow(x, exponent));
         }
         
-        Monomial& operator+(Monomial& other) {
-            if(other.exponent != this->exponent){
-                throw "Incompatible exponents!";
-            } else {
-                Monomial ret = Monomial((this->coeff + other.coeff), this->exponent);
-                return ret;
-            }
+        Expr operator+(Monomial& other);
+
+        double exp() {
+            return this->exponent;
+        }
+
+        double coeff() {
+            return this->_coeff;
         }
 
     };
 
-    class Polynomial {
+    class Polynomial : public Expr {
+
+    // sum of monomials for more complex expressions
+
     private:
         std::map<double, Monomial> monos;
 
@@ -53,6 +62,63 @@ namespace Algebra {
             return sigma;
         }
 
+        void insert(Monomial m) {
+            this->monos.emplace(m.exp(), m);
+        }
+
+        Expr operator+(Monomial m) {
+            if(this->monos.count(m.exp())) {
+                Monomial n = monos.at(m.exp());
+                this->monos.erase(m.exp()); // better solution possible here
+                this->monos.emplace(m.exp(), Monomial(m.exp(), n.coeff() + m.coeff()));
+                return *this;
+            }
+        }
     };
+
+    Expr Monomial::operator+(Monomial& other) {
+        if(other.exponent != this->exponent){
+            Polynomial poly;
+            poly.insert(*this);
+            poly.insert(other);
+            return poly;
+        } else {
+            Monomial mon = Monomial((this->_coeff + other._coeff), this->exponent);
+            return mon;
+        }
+    }
     
+}
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Monomial test: " << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    Algebra::Monomial m2x3 = Algebra::Monomial(2, 3);
+
+    std::cout << "2x^3 at 2 - Expect 16: " << m2x3.evaluateAt(2) << std::endl;
+    std::cout << "2x^3 at 3 - Expect 54: " << m2x3.evaluateAt(3) << std::endl;
+    std::cout << "2x^3 at 2.2 - Expect 21.296...: " << m2x3.evaluateAt(2.2) << std::endl;
+
+    Algebra::Monomial m2_2x3 = Algebra::Monomial(2.2, 3);
+
+    std::cout << "2.2x^3 at 2 - Expect 17.6: " << m2_2x3.evaluateAt(2) << std::endl;
+    std::cout << "2.2x^3 at 3 - Expect 59.4: " << m2_2x3.evaluateAt(3) << std::endl;
+    std::cout << "2.2x^3 at 2.2 - Expect 23.42...: " << m2_2x3.evaluateAt(2.2) << std::endl;
+
+    Algebra::Monomial m2x2_8 = Algebra::Monomial(2, 2.8);
+
+    std::cout << "2x^2.8 at 2 - Expect 13.928...: " << m2x2_8.evaluateAt(2) << std::endl;
+    std::cout << "2x^2.8 at 3 - Expect 43.348...: " << m2x2_8.evaluateAt(3) << std::endl;
+    std::cout << "2x^2.8 at 2.2 - Expect 18.189...: " << m2x2_8.evaluateAt(2.2) << std::endl;
+    
+    std::cout << "---------------------------------" << std::endl << std::endl;
+
+    std::cout << "Polynomial Test:" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    
+
+    return 0;
 }
